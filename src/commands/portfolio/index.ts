c29ae8001d1f15fced8662810ead30a1ac0dbe53
Hao (Steven) <nguyenphuhao.it@gmail.com>
2021-12-27 20:31:59 +0700

    Clean code

--- src/commands/portfolio/index.ts
+++ src/commands/portfolio/index.ts
@@ -2,7 +2,6 @@ import { Command, Flags } from "@oclif/core";
 import isEmpty from "lodash/isEmpty";
 import calculateBalance from "../../portfolio/calculateBalance";
 import getTokenValues from "../../portfolio/getTokenValues";
-import { TokenBalanceMap } from "../../portfolio/types";
 
 export default class GetPortfolioCommand extends Command {
   static description = "return the latest portfolio value per token in USD";
